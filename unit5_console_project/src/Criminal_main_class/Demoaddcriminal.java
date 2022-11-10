22ab38e8ff36f7aae1a5ad323a52d06cea1812ba
Padmanabh2 <desaipadmanabh191@gmail.com>
2022-11-11 00:16:47 +0530

    adding day2

---
+++ unit5_console_project/src/Criminal_main_class/Demoaddcriminal.java
@@ -0,0 +1,10 @@
+package Criminal_main_class;
+
+public class Demoaddcriminal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ unit5_console_project/src/Dbutil/Intr_criminaldetails_impl.java
@@ -0,0 +1,23 @@
+package Dbutil;
+
+public class Intr_criminaldetails_impl implements Intr_criminaldetails {
+
+	@Override
+	public String addcriminaldetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String editcriminaldetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deletecriminaldetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
